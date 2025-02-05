d704fbd2a860c7b137f134ed11612a0786a6c6c6
galkinaksenia <ksun2001@rambler.ru>
2025-02-05 10:38:51 +0300

    correct key

--- petition_classification/settings.py
+++ petition_classification/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-ECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 if os.environ.get("DEBUG") == "False":
     DEBUG = False
